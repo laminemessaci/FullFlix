39efb0ec623ff9a4e780ec4007c15705473c9d12
laminemessaci <lamine76@admins-macbook-pro.home>
2021-09-27 10:18:40 +0200

    Add logger for Redux console

--- src/store/ConfigureStore.js
+++ src/store/ConfigureStore.js
@@ -1,8 +1,9 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 //import {composeWithDevTools} from 'redux-devtools-extensions';
 import addToFavorites from './reducers';
 
 //const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
 
-export default createStore(addToFavorites);
+export default createStore(addToFavorites, applyMiddleware(logger));
